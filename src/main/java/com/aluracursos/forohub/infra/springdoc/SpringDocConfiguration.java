7b032303268b636c3cfa55a24df61a74760ef3ce
mendodevv <mendodevcs@gmail.com>
2024-07-08 20:22:16 -0600

    Documentation addons

---
+++ src/main/java/com/aluracursos/forohub/infra/springdoc/SpringDocConfiguration.java
@@ -0,0 +1,26 @@
+package com.aluracursos.forohub.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("API Desafío-Forohub")
+                        .description("API Rest del desafío forohub, que contiene las funcionalidades de CRUD para tópicos dentro del foro.")
+                        .version("1.0.0"));
+    }
+}
